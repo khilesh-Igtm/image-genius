9d17ddca3a93e53bb2b1769a8f613cb5809f7798
khilesh <khileshsingh5678@gmail.com>
2024-02-22 21:32:43 +0530

    Updated again

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -1,4 +1,4 @@
-"use server";
+"use server"; //this file contains many action related to server work
 
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database/mongoose";
@@ -15,11 +15,11 @@ const populateUser = (query: any) => query.populate({
   select: '_id firstName lastName clerkId'
 })
 
-// ADD IMAGE
+// Add Image
+
 export async function addImage({ image, userId, path }: AddImageParams) {
   try {
     await connectToDatabase();
-
     const author = await User.findById(userId);
 
     if (!author) {
@@ -31,19 +31,21 @@ export async function addImage({ image, userId, path }: AddImageParams) {
       author: author._id,
     })
 
-    revalidatePath(path);
 
+
+
+    revalidatePath(path);
     return JSON.parse(JSON.stringify(newImage));
   } catch (error) {
     handleError(error)
   }
 }
 
-// UPDATE IMAGE
+// Update Image
+
 export async function updateImage({ image, userId, path }: UpdateImageParams) {
   try {
     await connectToDatabase();
-
     const imageToUpdate = await Image.findById(image._id);
 
     if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
@@ -53,39 +55,38 @@ export async function updateImage({ image, userId, path }: UpdateImageParams) {
     const updatedImage = await Image.findByIdAndUpdate(
       imageToUpdate._id,
       image,
-      { new: true }
+      {
+        new: true
+      }
     )
-
     revalidatePath(path);
-
     return JSON.parse(JSON.stringify(updatedImage));
   } catch (error) {
     handleError(error)
   }
 }
 
-// DELETE IMAGE
+// Delete Image
+
 export async function deleteImage(imageId: string) {
   try {
     await connectToDatabase();
-
     await Image.findByIdAndDelete(imageId);
   } catch (error) {
     handleError(error)
-  } finally{
+  } finally {
     redirect('/')
   }
 }
 
-// GET IMAGE
+// Get Image
+
 export async function getImageById(imageId: string) {
   try {
     await connectToDatabase();
 
-    const image = await populateUser(Image.findById(imageId));
-
-    if(!image) throw new Error("Image not found");
-
+    const image = await populateUser(Image.findById(imageId))
+    if (!image) throw new Error("Image not found");
     return JSON.parse(JSON.stringify(image));
   } catch (error) {
     handleError(error)
@@ -93,6 +94,7 @@ export async function getImageById(imageId: string) {
 }
 
 // GET IMAGES
+
 export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
   limit?: number;
   page: number;
@@ -100,7 +102,6 @@ export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
 }) {
   try {
     await connectToDatabase();
-
     cloudinary.config({
       cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
@@ -108,21 +109,19 @@ export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
       secure: true,
     })
 
-    let expression = 'folder=imaginify';
+    let expresssion = 'folder=imgGenius';
 
     if (searchQuery) {
-      expression += ` AND ${searchQuery}`
+      expresssion += `AND ${searchQuery}`
     }
 
     const { resources } = await cloudinary.search
-      .expression(expression)
-      .execute();
+      .expression(expresssion).execute();
 
     const resourceIds = resources.map((resource: any) => resource.public_id);
 
     let query = {};
-
-    if(searchQuery) {
+    if (searchQuery) {
       query = {
         publicId: {
           $in: resourceIds
@@ -130,13 +129,10 @@ export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
       }
     }
 
-    const skipAmount = (Number(page) -1) * limit;
+    const skipAmount = (Number(page) - 1) * limit;
 
-    const images = await populateUser(Image.find(query))
-      .sort({ updatedAt: -1 })
-      .skip(skipAmount)
-      .limit(limit);
-    
+    const images = await populateUser(Image.find(query)).sort({ updatedAt: -1 }).skip(skipAmount).limit(limit);
+
     const totalImages = await Image.find(query).countDocuments();
     const savedImages = await Image.find().countDocuments();
 
@@ -145,6 +141,8 @@ export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
       totalPage: Math.ceil(totalImages / limit),
       savedImages,
     }
+
+
   } catch (error) {
     handleError(error)
   }
--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,14 +1,15 @@
 import { Document, Schema, model, models } from "mongoose";
 
+
 export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureURL: string; 
+  secureURL: string;
   width?: number;
   height?: number;
-  config?: object; 
-  transformationUrl?: string; 
+  config?: object;
+  tranformationUrl?: string;
   aspectRatio?: string;
   color?: string;
   prompt?: string;
@@ -21,9 +22,10 @@ export interface IImage extends Document {
   updatedAt?: Date;
 }
 
+
 const ImageSchema = new Schema({
   title: { type: String, required: true },
-  transformationType: { type: String, required: true },
+  tranformationType: { type: String, required: true },
   publicId: { type: String, required: true },
   secureURL: { type: String, required: true },
   width: { type: Number },
@@ -36,7 +38,7 @@ const ImageSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
-});
+})
 
 const Image = models?.Image || model('Image', ImageSchema);
 
